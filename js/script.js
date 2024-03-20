111385276bb8ce3850d1386ff64a32695887ec78
Ireneusz Miernik <irekmiernik@gmail.com>
2024-03-20 22:00:01 +0100

    The DOM is updated instead of HTML elements

--- js/script.js
+++ js/script.js
@@ -1,16 +1,25 @@
 (() => {
-    const renderTasksList = (table) => {
-        let htmlString = "";
-        for (const tabElement of table) {
+    const updateDOM = (table) => {
+        const div = document.querySelector(".js-div");
+        document.querySelector(".js-tasksList").remove();
+        const tasksList = document.createElement("ul");
+        tasksList.setAttribute("class", "js-tasksList");
+        div.appendChild(tasksList);
 
-            htmlString += `
-            <li ${tabElement.done ? "style=\"text-decoration: line-through\"" : ""}>
-                <button class="js-doneTask">Wykonane?</button>
-                ${tabElement.content}
-                <button class="js-deleteTask">Usuń</button>
-            </li>`;
-        }
-        document.querySelector(".js-taskList").innerHTML = htmlString;
+        for (const tabElement of table) {
+            const listItem = document.createElement("li");
+            if (tabElement.done) listItem.setAttribute("style", "text-decoration:line-through");
+            tasksList.appendChild(listItem);
+            const doneItem = document.createElement("button");
+            doneItem.setAttribute("class", "js-doneTask");
+            doneItem.innerText = "Wykonane?";
+            listItem.append(doneItem);
+            listItem.append(`${tabElement.content}`);
+            const deleteItem = document.createElement("button");
+            deleteItem.setAttribute("class", "js-deleteTask");
+            deleteItem.textContent = "Usuń?";
+            listItem.appendChild(deleteItem);
+        };
         deleteTasks(table);
         toggleDoneTask(table);
     };
@@ -23,7 +32,7 @@
             if (newTaskContent.value.trim() === "") return;
             table.push({ content: newTaskContent.value.trim(), });
             newTaskContent.value = "";
-            renderTasksList(table);
+            updateDOM(table);
         });
     };
 
@@ -32,7 +41,7 @@
         doneButtons.forEach((doneTask, index) => {
             doneTask.addEventListener("click", () => {
                 table[index].done = table[index].done ? false : true;
-                renderTasksList(table);
+                updateDOM(table);
             });
         });
     };
@@ -42,7 +51,7 @@
         deleteButtons.forEach((deleteTask, index) => {
             deleteTask.addEventListener("click", () => {
                 table.splice(index, 1);
-                renderTasksList(table);
+                updateDOM(table);
             });
         });
     }
